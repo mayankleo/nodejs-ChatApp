b480f0eb6d5cbfb9c70a2e3faa96f37380957c32
mayank kushwaha <mayankkushwaha226@.com>
2022-03-03 16:32:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+const cors = require("cors");
+var app = express();
+app.use(cors());
+var http = require("http").createServer(app);
+var io = require("socket.io")(http,{
+  cors:{
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+var server_port = process.env.PORT || 3000;
+const onchat = [];
+
+app.use(express.static(__dirname + "/public"));
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.get("/", function (req, res) {res.sendFile(__dirname + "/chat.html");});
+
+io.on("connection", function (socket) {
+  console.log(socket.id + " Online!!!");
+  onchat.push(socket.id);
+
+  socket.on("disconnect", function () {
+    console.log(socket.id + " Disconnected!!!");
+    var userid = onchat.indexOf(socket.id);
+    onchat.splice(userid, 1);
+  });
+
+  socket.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  socket.on("base64", function (msgdata) {
+    console.log("image file from > " + msgdata.uname);
+    socket.broadcast.emit("base64", msgdata);
+  });
+
+  socket.on("transmit_message", function (msg) {
+    console.log(socket.id + " : " + msg);
+    socket.broadcast.emit("recive_message", msg);
+  });
+
+  socket.on("typing", (data) => {
+    if (data.typing == true) {
+      io.emit("display", data);
+    } else {
+      io.emit("display", data);
+    }
+  });
+});
+
+http.listen(server_port, function () {
+  console.log("localhost:" + server_port);
+});
